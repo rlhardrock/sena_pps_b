652d5a0749a0a76234132fd716715ba321109e66
Louis Rey <rlhardrock@gmail.com>
2025-03-03 23:16:24 -0500

    add use.useGlobal ValidationPipes

--- .history/src/main_20250303230814.ts
+++ .history/src/main_20250303230814.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 
 async function bootstrap() {
